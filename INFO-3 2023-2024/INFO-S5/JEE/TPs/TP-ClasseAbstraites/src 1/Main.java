178c7d4bb66e740ea67047c77951ac873246f3fb
oulaydi <oussama.oulaydi@gmail.com>
2023-11-17 16:10:24 +0100

    remove copy

--- INFO-3 2023-2024/INFO-S5/JEE/TPs/TP-ClasseAbstraites/src 1/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        ProduitElectronique peq1 = new ProduitElectronique("apple", 1200, 10, 5);
-
-        peq1.calculerPrix();
-
-    }
-}
